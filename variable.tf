ee11236b87c40d940972a18a6e0f648be633b374
vijayw19 <62520617+vijayw19@users.noreply.github.com>
2023-10-02 11:42:06 -0400

    Adding username and password variable

--- variable.tf
+++ variable.tf
@@ -42,6 +42,16 @@ variable "username" {
   type        = string
 }
 
+variable "admin_username" {
+  description = "Admin username for the VM"
+  type        = string
+}
+
+variable "admin_password" {
+  description = "Admin password for the VM"
+  type        = string
+}
+
 variable "linux_vms" {
   description = "Linux VMs"
   type = object({
