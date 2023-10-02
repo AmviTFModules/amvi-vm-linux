8b9572e2635f3cd194735d99afbbec97def0bda2
vijayw19 <62520617+vijayw19@users.noreply.github.com>
2023-10-02 00:43:21 -0400

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,4 +1,4 @@
 output "vm_public_ip" {
-  description = "Public IP address of the Linux VM."
-  value       = azurerm_linux_virtual_machine.this.network_interface_ids[0]
+  description = "Public IP address of the Linux VM"
+  value       = azurerm_linux_virtual_machine.this[0].network_interface_ids[0]
 }
